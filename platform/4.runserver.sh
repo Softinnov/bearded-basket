606d000c95e382ef02fce4c5952e4276b0e88768
hamdouni <brahim@hamdouni.com>
2014-10-09 20:27:37 +0200

    more flexible go run server

--- platform/4.runserver.sh
+++ platform/4.runserver.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
 # RUN SERVER GO
-# usage: runserver <cheyip> <dbip> <dbuser> <dbpass>
+# usage: runserver <cheyip> <db container> <dbuser> <dbpass> <logdir>
+# example: runserver.sh localhost db admin admin $(pwd)/logs
 
-if [ $# -ne 4 ]; then
-	echo "Usage: $0 <cheyip> <dbip> <dbuser> <dbpass>"
+if [ $# -ne 5 ]; then
+	echo "Usage: $0 <cheyip> <db container> <dbuser> <dbpassa <logdir>"
 	exit 1
 fi
+
+CHEYIP=$1
+DBNAME=$2
+DBUSER=$3
+DBPASS=$4
+LOGDIR=$5
+
+DBIP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $DBNAME`
+
 
 echo ">> Stop it if running"
 killall server
 
 cd ../server
 go build && \
-	./server -db "$3:$4@($2:3306)/prod" -chey "http://$1:8000" &> ../platform/logs/go.log &
+	./server -db "$DBUSER:$DBPASS@($DBIP:3306)/prod" -chey "http://$CHEYIP:8000" &> $LOGDIR/go.log &
 echo ">> Go server running"
