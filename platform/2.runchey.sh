00bde0574370f1247389588f94a6ba737f9babf2
hamdouni <brahim@hamdouni.com>
2014-10-13 14:17:54 +0200

    externalize cheyenne log dir as parameter

--- platform/2.runchey.sh
+++ platform/2.runchey.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # RUN CHEYENNE WEBAPP
-# usage: runchey <absolute path to cheyenne webapp>
+# usage: runchey <absolute path to cheyenne webapp> <log dir>
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <absolute path to cheyenne webapp>"
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <absolute path to cheyenne webapp> <log dir>"
 	exit 1
 fi
 
@@ -13,5 +13,5 @@ echo ">> Removing old container (stop it if running)"
 
 echo ">> Running the cheyenne container with ANDES path in $1"
 echo ">> linked with db"
-docker run --name chey --link db:db -v $1:/ANDES -d softinnov/chey || exit $?
+docker run --name chey --link db:db -v $1:/ANDES -v $2:/var/log -d softinnov/chey || exit $?
 
