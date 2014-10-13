333a6d4376d1185858620fac326c5a40264f0ffb
hamdouni <brahim@hamdouni.com>
2014-10-13 14:21:12 +0200

    fix #15 accessing application with something different from localhost, also backend always request nginx not cheyenne directly

--- platform/4.runserver.sh
+++ platform/4.runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # RUN SERVER GO
-# usage: runserver <cheyip> <db container> <dbuser> <dbpass> <logdir>
+# usage: runserver <nginx> <db container> <dbuser> <dbpass> <logdir>
 # example: runserver.sh localhost db admin admin $(pwd)/logs
 
 if [ $# -ne 5 ]; then
@@ -9,7 +9,7 @@ if [ $# -ne 5 ]; then
 	exit 1
 fi
 
-CHEYIP=$1
+NGINX=$1
 DBNAME=$2
 DBUSER=$3
 DBPASS=$4
@@ -23,5 +23,5 @@ killall server
 
 cd ../server
 go build && \
-	./server -db "$DBUSER:$DBPASS@($DBIP:3306)/prod" -chey "http://$CHEYIP:8000" &> $LOGDIR/go.log &
+	./server -db "$DBUSER:$DBPASS@($DBIP:3306)/prod" -chey "http://$NGINX:8000" &> $LOGDIR/go.log &
 echo ">> Go server running"
