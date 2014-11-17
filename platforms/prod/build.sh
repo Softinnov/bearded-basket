6b3b165a2e1cbc4e9ae9eeb08197b0029730b977
Ghislain Guiot <gt.ghislain@gmail.com>
2014-11-17 15:29:34 +0100

    fix client missing folder

--- platforms/prod/build.sh
+++ platforms/prod/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo ">> Building db image..."
+cd db || exit $?
+docker build -t softinnov/prod-db . || exit $?
+cd ..
+echo ">> db image done."
+
 echo ">> Building cheyenne image..."
 cd chey || exit $?
 git submodule init
@@ -8,7 +14,16 @@ docker build -t softinnov/prod-chey . || exit $?
 cd ..
 echo ">> cheyenne image done."
 
+echo ">> Building back image..."
+cd back || exit $?
+./compile.sh || exit $?
+docker build -t softinnov/prod-back . || exit $?
+cd ..
+echo ">> back image done."
+
 echo ">> Building client image..."
+cd client || exit $?
 docker build -t softinnov/prod-client . || exit $?
 cd ..
 echo ">> client image done."
+
