89f4a98752b885ea10b04e7ffa16fdadd07a1326
Ghislain Guiot <gt.ghislain@gmail.com>
2014-11-18 17:28:26 +0100

    fix some bug, still broken

--- platforms/prod/install.sh
+++ platforms/prod/install.sh
@@ -6,11 +6,11 @@ curl -sSL https://get.docker.com/ubuntu/ | sudo sh || exit $?
 
 docker run --rm busybox echo "everything works" || exit $?
 
-docker run -d --volumes-from dbdata --name db softinnov/prod-db || exit $?
+docker run -d --volumes-from dbdata --name prod-db softinnov/prod-db || exit $?
 
-docker run -d --link db:db -v $(pwd)/logs:/var/log --name chey softinnov/prod-chey || exit $?
+docker run -d --link prod-db:db -v $(pwd)/logs:/var/log --name prod-chey softinnov/prod-chey || exit $?
 
-docker run -d --link db:db -v $(pwd)/logs:/logs --name back softinnov/prod-back || exit $?
+docker run -d --link prod-db:db -v $(pwd)/logs:/logs --name prod-back softinnov/prod-back || exit $?
 
-docker run -d -v $(pwd)/logs:/var/log/nginx --link chey:chey --link back:back -p 8000:8000 --name client softinnov/prod-client || exit $?
+docker run -d --link prod-chey:chey --link prod-back:back -v $(pwd)/logs:/var/log/nginx -p 8000:8000 --name prod-client softinnov/prod-client || exit $?
 
