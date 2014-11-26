89cd7d85993e47bf0c41e4f321aeaa981bc72d3c
Ghislain Guiot <gt.ghislain@gmail.com>
2014-11-26 18:19:29 +0100

    add update dbdata

--- platforms/prod/scripts/update.sh
+++ platforms/prod/scripts/update.sh
@@ -17,13 +17,13 @@ docker load -i prod-chey.tar   || exit $?
 docker load -i prod-back.tar   || exit $?
 docker load -i prod-client.tar || exit $?
 
-PROD[0]="docker run -d --volumes-from dbdata --name prod-db softinnov/prod-db"
+PROD[0]="docker run -d --volumes-from dbdata -v $(pwd)/data/:/data --name prod-db softinnov/prod-db"
 OLD[0]="prod-db"
 PROD[1]="docker run -d --link prod-db:db -v $(pwd)/logs:/var/log --name prod-chey softinnov/prod-chey"
 OLD[1]="prod-chey"
 PROD[2]="docker run -d --link prod-db:db -v $(pwd)/logs:/logs --name prod-back softinnov/prod-back"
 OLD[2]="prod-back"
-PROD[3]="docker run -d --link prod-chey:chey --link prod-back:back -v $(pwd)/logs:/var/log/nginx -p 8000:8000 --name prod-client softinnov/prod-client"
+PROD[3]="docker run -d --link prod-chey:chey --link prod-back:back -v $(pwd)/logs:/var/log/nginx -p 80:8000 -p 443:443 --name prod-client softinnov/prod-client"
 OLD[3]="prod-client"
 
 for i in {0..3}; do
