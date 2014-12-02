8aa5288506ddb8d8528c7dca56401577cef6e05a
Ghislain Guiot <gt.ghislain@gmail.com>
2014-12-02 15:07:41 +0100

    add gzip dbdata

--- platforms/prod/db/update_dbdata.sh
+++ platforms/prod/db/update_dbdata.sh
@@ -9,6 +9,8 @@ MYSQL_PASS=admin
 for ARG in $DBTABLES
 do
 	echo "  -> importing $ARG"
+	gunzip /data/"$ARG".txt.gz
 	cat /data/"$ARG".sql | mysql -u$MYSQL_USER -p$MYSQL_PASS $DBNAME || exit $?
 	mysqlimport --local -u$MYSQL_USER -p$MYSQL_PASS $DBNAME /data/"$ARG".txt || exit $?
+	gzip /data/"$ARG".txt
 done
